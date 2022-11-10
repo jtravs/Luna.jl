6972ceff3c08a70f02fef4c5ac61062fd2565894
John Travers <jtravs@gmail.com>
2022-11-10 19:43:42 +0000

    add max option to fwhm_t

--- src/Processing.jl
+++ src/Processing.jl
@@ -224,15 +224,17 @@ end
 Extract the temporal FWHM. If `bandpass` is given, bandpass the fieldaccording to
 [`window_maybe`](@ref). If `oversampling` > 1, the  time-domain field is oversampled before
 extracting the FWHM. If `sumdims` is given, the time-domain power is summed over these
-dimensions (e.g. modes) before extracting the FWHM.
+dimensions (e.g. modes) before extracting the FWHM. `minmax` determines if the `:min` or `:max`
+FWHM is taken.
 """
-function fwhm_t(grid::AbstractGrid, Eω; bandpass=nothing, oversampling=1, sumdims=nothing)
+function fwhm_t(grid::AbstractGrid, Eω; bandpass=nothing, oversampling=1, sumdims=nothing,
+                minmax=:min)
     to, Eto = getEt(grid, Eω; oversampling=oversampling, bandpass=bandpass)
     Pt = abs2.(Eto)
     if !isnothing(sumdims)
         Pt = dropdims(sum(Pt; dims=sumdims); dims=sumdims)
     end
-    fwhm(to, Pt)
+    fwhm(to, Pt; minmax)
 end
 
 function fwhm_t(output::AbstractOutput; kwargs...)
@@ -258,16 +260,16 @@ function fwhm_f(grid::AbstractGrid, Eω; bandpass=nothing, oversampling=1, sumdi
 end
 
 
-function fwhm(x, I)
+function fwhm(x, I; minmax=:min)
     out = Array{Float64, ndims(I)-1}(undef, size(I)[2:end])
     cidcs = CartesianIndices(size(I)[2:end])
     for ii in cidcs
-        out[ii] = fwhm(x, I[:, ii])
+        out[ii] = fwhm(x, I[:, ii]; minmax)
     end
     out
 end
 
-fwhm(x::Vector, I::Vector) = Maths.fwhm(x, I)
+fwhm(x::Vector, I::Vector; minmax=:min) = Maths.fwhm(x, I; minmax)
 
 """
     peakpower(grid, Eω; bandpass=nothing, oversampling=1)
